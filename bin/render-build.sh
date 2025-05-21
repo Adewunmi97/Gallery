7095ce8e0fbab9acbf7ae339ba271b1ad3d1d1dc
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-05-21 12:46:56 +0100

    Fix: run Solid Queue setup in Render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 # Exit on error
+# set -o errexit
+
+# bundle install
+# bin/rails assets:precompile
+# bin/rails assets:clean
+
+#!/usr/bin/env bash
 set -o errexit
 
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
+bundle exec solid_queue database setup
+bundle exec rails db:migrate
 
 # If you have a paid instance type, we recommend moving
 # database migrations like this one from the build command
 # to the pre-deploy command:
-bin/rails db:migrate
+# bin/rails db:migrate
